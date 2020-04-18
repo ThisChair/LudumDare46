825f935960eef8166221778b13761daadb6db521
Carlos Infante <carlosinfante1220@gmail.com>
2020-04-18 18:08:51 -0400

    Added movement system and player animation

--- Player.gd
+++ Player.gd
@@ -1,16 +1,33 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const WALK_SPEED = 250
+const WALK_ACC = 15
+const GRAVITY = 15
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 400
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
+var velocity = Vector2()
 
 func _physics_process(delta):
-	pass
+	velocity.y += GRAVITY
+	if (Input.is_action_pressed("ui_left")):
+		$Sprite.flip_h = true
+		velocity.x = max(velocity.x-WALK_ACC, -WALK_SPEED)
+		$Sprite.play("Walk")
+	elif (Input.is_action_pressed("ui_right")):
+		$Sprite.flip_h = false
+		velocity.x = min(velocity.x+WALK_ACC, WALK_SPEED)
+		$Sprite.play("Walk")
+	elif is_on_floor():
+		velocity.x = lerp(velocity.x, 0, 0.15)
+		$Sprite.play("Idle")
+	else:
+		velocity.x = lerp(velocity.x, 0, 0.025)
+		$Sprite.play("Idle")
+	if is_on_floor():
+		if Input.is_action_just_released("ui_accept"):
+			velocity.y = -JUMP_SPEED
+	else:
+		$Sprite.play("Idle")
+	velocity = move_and_slide(velocity, UP)
